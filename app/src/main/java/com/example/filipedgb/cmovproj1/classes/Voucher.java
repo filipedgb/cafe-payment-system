4252863255a00bc8dc776f01e54c6d1f2a302e54
filipedgb <filipedgb@gmail.com>
2016-11-06 15:12:29 +0000

    Vouchers with signature from nodejs

--- app/src/main/java/com/example/filipedgb/cmovproj1/classes/Voucher.java
+++ app/src/main/java/com/example/filipedgb/cmovproj1/classes/Voucher.java
@@ -1,35 +1,59 @@
 package com.example.filipedgb.cmovproj1.classes;
 
+import org.apache.commons.lang3.RandomUtils;
+
+import java.util.Random;
+
 /**
  * Created by Filipe Batista on 24/10/2016.
  */
 
 public class Voucher {
 
-
-    private long id;
     private int type; // 0 - coffe / 1 - popcorn / 2 - 5% discount
     private String user_id;
     private boolean used;
 
+    private String criptographic_signature;
+    private int serial;
+    private boolean signed;
+
 
     public Voucher() {
-        used = false;
+
     }
 
     public Voucher(String user_id_in,int type_in) {
         this.user_id = user_id_in;
         this.type = type_in;
-        used = false;
+        this.used = false;
+        this.serial = RandomUtils.nextInt(1,1000)+ RandomUtils.nextInt(100,500);
+        this.signed = false;
+        this.criptographic_signature = "";
     }
 
-
-    public long getId() {
-        return id;
+    public String getCriptographic_signature() {
+        return criptographic_signature;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setCriptographic_signature(String criptographic_signature) {
+        this.criptographic_signature = criptographic_signature;
+    }
+
+    public int getSerial() {
+        return serial;
+    }
+
+    public void setSerial(int serial) {
+        this.serial = serial;
+    }
+
+    public boolean isSigned() {
+        return signed;
+    }
+
+    public void setSigned(boolean signed) {
+        this.signed = signed;
     }
 
     public int getType() {
